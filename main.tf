201935fb47a0f5ace65c46ba5b093c5b4d6f5e64
Daniel Long <24.daniel.long@gmail.com>
2022-09-16 09:30:40 -0500

    Removes hard-coded credential variables from mongo provider

--- main.tf
+++ main.tf
@@ -7,10 +7,7 @@ terraform {
   }
 }
 
-provider "mongodbatlas" {
-  public_key  = var.mongodbatlas_public_key
-  private_key = var.mongodbatlas_private_key
-}
+provider "mongodbatlas" {}
 
 provider "aws" {
   region = var.aws_region
