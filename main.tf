1954e0feacbc344a64cef0477f8a4cd7954b8164
Daniel Long <24.daniel.long@gmail.com>
2022-09-16 09:36:23 -0500

    Fixes incorrect region variable

--- main.tf
+++ main.tf
@@ -222,5 +222,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${var.aws_account_id}:${data.tfe_outputs.api_gateway.values.gateway_id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.communities.path}"
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${data.tfe_outputs.api_gateway.values.gateway_id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.communities.path}"
 }
