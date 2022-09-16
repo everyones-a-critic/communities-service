508f90cf72bfba588c022fab7aab570e70ace3c7
Daniel Long <24.daniel.long@gmail.com>
2022-09-16 11:00:46 -0500

    Updates mongo uri to a environment variable

--- initial_data_load.py
+++ initial_data_load.py
@@ -6,7 +6,7 @@ import os
 def load_data():
     ca = certifi.where()
     client = pymongo.MongoClient(
-        f"mongodb+srv://cli_user:{os.environ.get('MONGO_PASSWORD')}@eac-ratings-dev.xtpcyty.mongodb.net/?retryWrites=true&w=majority",
+        f"mongodb+srv://cli_user:{os.environ.get('MONGO_PASSWORD')}@{os.environ.get('MONGO_URL_SNIP')}.mongodb.net/?retryWrites=true&w=majority",
         tlsCAFile=ca
     )
 
