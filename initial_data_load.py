d72e4a89ff9ad9dc794afb675391f770667ea46d
Daniel Long <24.daniel.long@gmail.com>
2022-09-28 14:45:21 -0500

    Adds a new community and updates initial data load to check for existing communities before inserting, rather than dropping the entire table

--- initial_data_load.py
+++ initial_data_load.py
@@ -18,13 +18,19 @@ def load_data():
         {"display_name": "Golf Courses", "name": "Golf Course", "plural_name": "Golf Courses", "icon": "golf-ball", "primary_color": "004D43",  "secondary_color": "FFFCDF"},
         {"display_name": "Movies", "name": "Movie", "plural_name": "Movies", "icon": "clapperboard", "primary_color": "4808B0", "secondary_color": "FFFFFF"},
         {"display_name": "Beer", "name": "Beer", "plural_name": "Beers", "icon": "wheat-alt", "primary_color": "20353C", "secondary_color": "BDA571"},
+        {"display_name": "Cheese", "name": "Cheese", "plural_name": "Cheeses", "icon": "cheese", "primary_color": "C21319", "secondary_color": "F4C13A"},
     ]
 
     db = client['prod']
     community_coll = db.community
     community_coll.create_index("name")
-    community_coll.drop()
-    result = community_coll.insert_many(communities_to_insert)
+    existing_communities = set()
+    for community in community_coll.find():
+        existing_communities.add(community['name'])
+
+    for community in communities_to_insert:
+        if community['name'] not in existing_communities:
+            community_coll.insert_one(community)
 
     community_member = db.community_member
     community_member.create_index([("user_id", pymongo.ASCENDING), ("community_id", pymongo.ASCENDING)], unique=True)
