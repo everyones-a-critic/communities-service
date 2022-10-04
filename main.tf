52d7d43c68daf6e850a35072941771bf84f364b6
Daniel Long <24.daniel.long@gmail.com>
2022-10-04 15:10:10 -0500

    Updates improper input name

--- main.tf
+++ main.tf
@@ -32,12 +32,12 @@ data "tfe_outputs" "mongo_db" {
 
 # Creating a cluster specifically for communities-service
 resource "mongodbatlas_serverless_instance" "main" {
-  project_id   = data.tfe_outputs.mongo_db.values.project_id
-  name         = "communities-service-${var.environment}"
+  project_id = data.tfe_outputs.mongo_db.values.project_id
+  name       = "communities-service-${var.environment}"
 
   provider_settings_backing_provider_name = "AWS"
-  provider_settings_provider_name = "SERVERLESS"
-  region_name           = var.mongo_region
+  provider_settings_provider_name         = "SERVERLESS"
+  provider_settings_region_name           = var.mongo_region
 }
 
 # Creating mongo resources to grant AWS lambdas access to mongo clusters
@@ -97,12 +97,12 @@ resource "mongodbatlas_database_user" "admin" {
 
 # This errors silently and doesn't get created if no data has been loaded yet. TODO: figure out deployment sequence
 resource "mongodbatlas_search_index" "community_name" {
-  name   = "default"
-  project_id = data.tfe_outputs.mongo_db.values.project_id
-  cluster_name = mongodbatlas_serverless_instance.main.name
-  analyzer = "lucene.standard"
-  collection_name = "community"
-  database = "prod"
+  name             = "default"
+  project_id       = data.tfe_outputs.mongo_db.values.project_id
+  cluster_name     = mongodbatlas_serverless_instance.main.name
+  analyzer         = "lucene.standard"
+  collection_name  = "community"
+  database         = "prod"
   mappings_dynamic = true
 
   search_analyzer = "lucene.standard"
@@ -117,13 +117,13 @@ resource "aws_api_gateway_resource" "communities" {
 }
 
 module "get_communities_service" {
-  source = "./modules/api_gateway_lambda_service"
-  service_name     = "get-communities"
-  command          = "services.list_communities"
-  http_method = "GET"
-  gateway_resource = aws_api_gateway_resource.communities
-  lambda_role = aws_iam_role.mongo-atlas-access.arn
-  mongo_cluster_uri = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
+  source             = "./modules/api_gateway_lambda_service"
+  service_name       = "get-communities"
+  command            = "services.list_communities"
+  http_method        = "GET"
+  gateway_resource   = aws_api_gateway_resource.communities
+  lambda_role        = aws_iam_role.mongo-atlas-access.arn
+  mongo_cluster_uri  = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
   mongo_cluster_name = mongodbatlas_serverless_instance.main.name
 }
 
@@ -134,13 +134,13 @@ resource "aws_api_gateway_resource" "community_id" {
 }
 
 module "get_community_service" {
-  source = "./modules/api_gateway_lambda_service"
-  service_name     = "get-community"
-  command          = "services.get_community"
-  http_method = "GET"
-  gateway_resource = aws_api_gateway_resource.community_id
-  lambda_role = aws_iam_role.mongo-atlas-access.arn
-  mongo_cluster_uri = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
+  source             = "./modules/api_gateway_lambda_service"
+  service_name       = "get-community"
+  command            = "services.get_community"
+  http_method        = "GET"
+  gateway_resource   = aws_api_gateway_resource.community_id
+  lambda_role        = aws_iam_role.mongo-atlas-access.arn
+  mongo_cluster_uri  = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
   mongo_cluster_name = mongodbatlas_serverless_instance.main.name
 }
 
@@ -151,24 +151,24 @@ resource "aws_api_gateway_resource" "members" {
 }
 
 module "join_community" {
-  source = "./modules/api_gateway_lambda_service"
-  service_name     = "join-community"
-  command          = "services.join_community"
-  http_method = "POST"
-  gateway_resource = aws_api_gateway_resource.members
-  lambda_role = aws_iam_role.mongo-atlas-access.arn
-  mongo_cluster_uri = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
+  source             = "./modules/api_gateway_lambda_service"
+  service_name       = "join-community"
+  command            = "services.join_community"
+  http_method        = "POST"
+  gateway_resource   = aws_api_gateway_resource.members
+  lambda_role        = aws_iam_role.mongo-atlas-access.arn
+  mongo_cluster_uri  = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
   mongo_cluster_name = mongodbatlas_serverless_instance.main.name
 }
 
 module "leave_community" {
-  source = "./modules/api_gateway_lambda_service"
-  service_name     = "leave-community"
-  command          = "services.leave_community"
-  http_method = "DELETE"
-  gateway_resource = aws_api_gateway_resource.members
-  lambda_role = aws_iam_role.mongo-atlas-access.arn
-  mongo_cluster_uri = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
+  source             = "./modules/api_gateway_lambda_service"
+  service_name       = "leave-community"
+  command            = "services.leave_community"
+  http_method        = "DELETE"
+  gateway_resource   = aws_api_gateway_resource.members
+  lambda_role        = aws_iam_role.mongo-atlas-access.arn
+  mongo_cluster_uri  = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
   mongo_cluster_name = mongodbatlas_serverless_instance.main.name
 }
 
